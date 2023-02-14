3449e32e81c6af0624491e027201913a93a172b4
Mariano <marianonunezma@gmail.com>
2023-02-14 18:01:27 -0300

    Adding some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = ({ title, initialNumber }) => {
+  const [count, setCount] = useState(initialNumber);
+
+  const handleAdd = () => setCount(count + 1);
+  const handleSubtract = () => setCount(count - 1);
+  const handleReset = () => setCount(0);
 
   return (
     <>
-      <h1>Counter App with Vite</h1>
+      <h1>{title}</h1>
       <p>{count}</p>
       <div className="btn-container">
-        <button onClick={() => setCount(count + 1)}>+ 1</button>
-        <button onClick={() => setCount(0)}>Reset</button>
-        <button onClick={() => setCount(count - 1)}>- 1</button>
+        <button onClick={handleAdd}>+ 1</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleSubtract}>- 1</button>
       </div>
     </>
   );
+};
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+  initialNumber: PropTypes.number.isRequired,
+};
+
+App.defaultProps = {
+  title: 'Default Title',
+  initialNumber: 0,
 };
 
 export default App;
